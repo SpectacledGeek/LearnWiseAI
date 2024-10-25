7b326a1daae5e428cfe07a8d25f21ba03b5cfa4e
Tanush Salian <tanush1852@gmail.com>
2024-10-26 00:02:57 +0530

    signin and signup integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './screens/Home';
 import Login from './screens/Login';
+import Signup from './screens/Signup';
 function App() {
   const [count, setCount] = useState(0)
 
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
         
 
       </Routes>
