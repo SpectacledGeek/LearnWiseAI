15447e8c4c42fdac09111738f56cdcc67642a4b4
Aadi Shetty <106333491+gltichy678@users.noreply.github.com>
2024-10-26 07:54:48 +0530

    Refactor user.controller.js and conversation.model.js imports

--- backend/src/models/conversation.model.js
+++ backend/src/models/conversation.model.js
@@ -1,3 +1,8 @@
+import mongoose from "mongoose";
+
+
+
+
 const conversationSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
@@ -19,4 +24,5 @@ const conversationSchema = new mongoose.Schema({
   },
 });
 
-export const Conversation = mongoose.model("Conversation", conversationSchema);
+ const Conversation = mongoose.model("Conversation", conversationSchema);
+export default Conversation;
